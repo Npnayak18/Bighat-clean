52bf5a9cfdd93cd2ccbd04896e2885fe94565157
npnayak18 <nithyaprakash2164@gmail.com>
2026-04-30 17:05:14 +0530

    fix mail + orders

--- server/models/Order.js
+++ server/models/Order.js
@@ -1,34 +1,30 @@
 const mongoose = require("mongoose");
-
 const OrderSchema = new mongoose.Schema(
   {
     items: [
       {
-        name: {
-          type: String,
-          required: true,
+name: {
+type: String,
+required: true,
         },
-        price: {
-          type: Number,
-          required: true,
+price: {
+type: Number,
+required: true,
         },
-        qty: {
-          type: Number,
-          required: true,
+qty: {
+type: Number,
+required: true,
         },
       },
     ],
-
     totalAmount: {
       type: Number,
       required: true,
     },
-
     email: {
       type: String,
       required: true,
     },
-
     status: {
       type: String,
       default: "pending",
